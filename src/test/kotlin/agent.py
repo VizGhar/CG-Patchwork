1da3e81dffbe6df8ce67d4e324bda85517cf841b
Jan Kandrac <jan@kandrac.xyz>
2022-09-25 12:28:18 +0200

    Fixed default python Boss (skip only)

--- src/test/kotlin/agent.py
+++ src/test/kotlin/agent.py
@@ -11,14 +11,6 @@ patch_events = int(input())  # the amount of "Special Patch" events that will ha
 for i in input().split():
     # patch_time: when the "Special Patch" will happen
     patch_time = int(i)
-patches = int(input())  # how many patches there are in total in a game
-for i in range(patches):
-    inputs = input().split()
-    patch_id = int(inputs[0])
-    patch_earning = int(inputs[1])  # how much Buttons you will acquire for this patch when "Button income" timepoint is reached
-    patch_button_price = int(inputs[2])  # how much Buttons does it cost to buy this patch
-    patch_time_price = int(inputs[3])  # how much time does it take to sew this patch
-    patch_shape = inputs[4]  # representation of patch shape "O.O|OOO|O.O" represents H shaped patch
 
 # game loop
 while True:
@@ -34,14 +26,15 @@ while True:
     opponent_buttons, opponent_time, opponent_earning = [int(i) for i in input().split()]
     for i in range(9):
         line = input()
-    playable_patches = int(input())  # number of patches you can play (3 most of the time, 1 in case of "Special Patch" event)
-    for i in range(playable_patches):
+    patches = int(input())  # count of still not used patches (you can play only one of first 3)
+    for i in range(patches):
         inputs = input().split()
         patch_id = int(inputs[0])
-        patch_earning = int(inputs[1])
-        patch_button_price = int(inputs[2])
-        patch_time_price = int(inputs[3])
-        patch_shape = inputs[4]
+        patch_earning = int(inputs[1])  # how much Buttons you will acquire for this tile when "Button income" timepoint is reached
+        patch_button_price = int(inputs[2])  # how much Buttons does it cost to buy this patch
+        patch_time_price = int(inputs[3])  # how much time does it take to sew this patch
+        patch_shape = inputs[4]  # representation of patch shape "O.O|OOO|O.O" represents H shaped tile
+    bonus_patch_id = int(input())  # 0 if no bonus patch is available
 
     # Write an action using print
     # To debug: print("Debug messages...", file=sys.stderr, flush=True)
